043040f72679382e14d7a08e2a4460014d713c2b
Daniel Cazé <danielctlima@gmail.com>
2023-06-06 11:56:04 -0300

    :hammer: feat: movie card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:watch_me/components/movie-card.dart';
+import 'package:watch_me/models/movie.dart';
+import 'package:watch_me/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Watch-me'),
     );
   }
 }
@@ -30,37 +34,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Movie> movies = [
+    Movie(
+        id: Uuid(),
+        imageUrl: 'https://github.com/danielcaze.png',
+        title: 'Movie 1',
+        description: 'This is a description of movie 1.',
+        rating: 3.9,
+        ageRange: '14',
+        gender: 'HORROR',
+        runtime: const Duration(hours: 12),
+        year: 2012),
+    // Add more movies here...
+  ];
 
-  void _incrementCounter() {
+  bool isModalOpen = false;
+
+  void openModal() {
     setState(() {
-      _counter++;
+      isModalOpen = !isModalOpen;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: AppColors.background,
+        foregroundColor: AppColors.yellow,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          return MovieCard(
+            imageUrl: movies[index].imageUrl,
+            title: movies[index].title,
+            description: movies[index].description,
+            rating: movies[index].rating,
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: openModal,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
