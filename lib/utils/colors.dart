df991566fcd38f051773f5374c368407afa4ceb2
Daniel Cazé <danielctlima@gmail.com>
2023-06-06 11:55:17 -0300

    :hammer: feat: Colors constant

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0XFF1F2229);
+  static const Color background2 = Color(0XFF2E303C);
+  static const Color backgroundButton = Color(0XFF373945);
+  static const Color backgroundButton2 = Color(0XFF4B4D59);
+  static const Color yellow = Color(0XFFFAE800);
+  static const Color gray = Color(0XFFBEC2C6);
+  static const Color white = Color(0XFFFBFBFB);
+}
